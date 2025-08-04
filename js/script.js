f0dbf0ec1caf99fccc854e879f2d1625c5bc981e
Mern2025 <merndeveloper2025@gmail.com>
2025-08-05 03:20:59 +0600

    input and button design added

--- js/script.js
+++ js/script.js
@@ -1,17 +1,33 @@
 // **protektta function er vitore je first brakcet thake oitake ble perameter/
 // **perameter kaj hocche information catch kora
 
-let result = document.querySelector('.result')
+// let result = document.querySelector('.result')
 
-let myArray = ['Mouse', 'Monitor', 'Keyboard', 'Printer', 'Processor']
+// let myArray = ['Mouse', 'Monitor', 'Keyboard', 'Printer', 'Processor']
 
-myArray.map((item)=>{
-    let div = document.createElement('div')
-    div.classList.add('product_box')
-    result.appendChild(div)
-  return  result.innerHTML += item + '<br>'
+// myArray.map((item)=>{
+//     let div = document.createElement('div')
+//     div.classList.add('product_box')
+//     result.appendChild(div)
+//   return  result.innerHTML += item + '<br>'
+// })
+
+
+
+// -----***----------
+// filter and find method =>
+    // 1 first eitai loop er procces run hoi
+   //  2 kaj hocche order wise item search kora
+
+let input = document.querySelector('input')
+let button = document.querySelector('button')
+let shop = ['Mango', 'Apple', 'Banana', 'Jackfruit', 'Orange']
+
+//find method working item looping
+
+
+button.addEventListener('click',()=>{
+    let findData = shop.find((items)=>{
+        
+    })
 })
-
-
-
-  
