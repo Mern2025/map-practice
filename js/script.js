cee69fbe0ea049ce5a0864a24a08974366a41448
Mern2025 <merndeveloper2025@gmail.com>
2025-08-05 02:38:45 +0600

    map method syntex added

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// **protektta function er vitore je first brakcet thake oitake ble perameter/
+// **perameter kaj hocche information catch kora
+
+let result = document.querySelector('.result')
+
+let myArray = ['Mouse', 'Monitor', 'Keyboard', 'Printer']
+
+myArray.map((item)=>{
+    result.innerHTML = item
+})
+
+  
