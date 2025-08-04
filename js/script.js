4b6708a610891e93e050db1495c6d08f86e22752
Mern2025 <merndeveloper2025@gmail.com>
2025-08-05 02:49:14 +0600

    br tag added

--- js/script.js
+++ js/script.js
@@ -6,7 +6,9 @@ let result = document.querySelector('.result')
 let myArray = ['Mouse', 'Monitor', 'Keyboard', 'Printer']
 
 myArray.map((item)=>{
-    result.innerHTML = item
+  return  result.innerHTML += item + '<br>'
 })
+
+
 
   
